fb313afb965d18131e5790c9ddb91cd42c28dc02
Manuel Farrapeira <mfarrapeira@hotmail.com>
2025-06-07 08:22:52 +0200

    jacoco

--- codefm-domain/src/main/java/org/web/codefm/domain/entity/Usuario.java
+++ codefm-domain/src/main/java/org/web/codefm/domain/entity/Usuario.java
@@ -1,12 +1,10 @@
 package org.web.codefm.domain.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @Builder
+@Generated
 @NoArgsConstructor
 @AllArgsConstructor
 public class Usuario {
