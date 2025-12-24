e1b1e4a17d122504b06ca3fc0e4626f6a31298d5
Manuel Farrapeira Pérez <manuelfpere@ext.inditex.com>
2025-12-24 11:56:32 +0100

    add SQLRestriction annotation to ClassEntity for soft deletion handling

--- codefm-infrastructure/src/main/java/org/web/codefm/infrastructure/entity/mariadb/teachernotebook/ClassEntity.java
+++ codefm-infrastructure/src/main/java/org/web/codefm/infrastructure/entity/mariadb/teachernotebook/ClassEntity.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Generated;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.time.LocalDate;
 
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@SQLRestriction("deletion_date is null")
 @Generated
 public class ClassEntity {
 
