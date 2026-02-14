fd9168045a09be401a6bbed8990cca296ab317eb
Manuel Farrapeira <mfarrapeira@hotmail.com>
2026-02-14 14:38:19 +0100

    add Gatling stress tests for API endpoints with configuration and reporting

--- gatling-test/src/test/java/org/web/codefm/gatling/AllEndpointsStressSimulation.java
+++ gatling-test/src/test/java/org/web/codefm/gatling/AllEndpointsStressSimulation.java
@@ -153,10 +153,10 @@ public class AllEndpointsStressSimulation extends Simulation {
     private PopulationBuilder injectUsers(ScenarioBuilder scenario) {
         return scenario.injectOpen(
                 nothingFor(Duration.ofSeconds(2)),
-                rampUsers(40).during(Duration.ofSeconds(15)),
-                constantUsersPerSec(8).during(Duration.ofSeconds(20)),
-                rampUsersPerSec(8).to(25).during(Duration.ofSeconds(15)),
-                constantUsersPerSec(25).during(Duration.ofSeconds(20))
+                rampUsers(40).during(Duration.ofSeconds(10)),
+                constantUsersPerSec(8).during(Duration.ofSeconds(15)),
+                rampUsersPerSec(8).to(22).during(Duration.ofSeconds(15)),
+                constantUsersPerSec(22).during(Duration.ofSeconds(15))
         );
     }
 
